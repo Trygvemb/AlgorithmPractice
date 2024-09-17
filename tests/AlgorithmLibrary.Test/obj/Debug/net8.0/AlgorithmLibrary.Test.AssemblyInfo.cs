f5c1685ebfb29eaba94ebf890c61be96390b9b39
trygvebechsgaard <trygvemb@gmail.com>
2024-09-17 08:09:08 +0700

    Update .gitignore to exclude test and temporary files

--- tests/AlgorithmLibrary.Test/obj/Debug/net8.0/AlgorithmLibrary.Test.AssemblyInfo.cs
+++ tests/AlgorithmLibrary.Test/obj/Debug/net8.0/AlgorithmLibrary.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmLibrary.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e895e126f96d278d3a390a7e3e312d2c2702772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6700b2f0b0d1889dd23a61a468a56874ee1d02f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmLibrary.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmLibrary.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
